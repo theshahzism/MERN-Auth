97c2dfd3aefc80c2ddb2a7ada4c9c5834b96aa45
Shahzain Hassan <shahzain.hassan.1@gmail.com>
2024-03-27 03:30:39 +0500

    created sign up api route

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRouter from "./routes/user.route.js";
+import userRoutes from "./routes/user.route.js"; 
+import authRoutes from "./routes/auth.route.js"; 
 
 dotenv.config();
 
@@ -16,8 +17,11 @@ mongoose
 
 const app = express();
 
+app.use(express.json())
+
 app.listen(3000, () => {
   console.log("server listening on port 3000!");
 });
 
-app.use("/api/user",userRouter);
+app.use("/api/user",userRoutes);
+app.use("/api/auth",authRoutes);

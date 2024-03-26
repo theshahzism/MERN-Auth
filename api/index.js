825cca3efcbc0c9dd7b5d2ef102fdc635e3cf3f7
Shahzain Hassan <shahzain.hassan.1@gmail.com>
2024-03-27 00:39:59 +0500

    connected to db

--- api/index.js
+++ api/index.js
@@ -1,6 +1,19 @@
-import express from 'express';
-const app=express();
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-app.listen(300,()=>{
-    console.log("server listening on port 3000")
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
+
+app.listen(300, () => {
+  console.log("server listening on port 3000!");
 });

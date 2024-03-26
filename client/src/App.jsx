e0ac5891e9be7a094f531fd7f1ce62763b2a2081
Shahzain Hassan <shahzain.hassan.1@gmail.com>
2024-03-26 23:39:11 +0500

    created Header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,12 @@ import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import About from "./pages/About";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <BrowserRouter>
+    <Header></Header>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
